c7d767606999fd03019569c4bbf60c337e02a9ea
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-07-01 17:06:55 +0200

    modified environment - upgraded to Python 3.10

--- mpi4py_cupy/example4.sh
+++ mpi4py_cupy/example4.sh
@@ -7,6 +7,7 @@ salloc -p dev_gpu_4 --gres=gpu:1 --ntasks 2 --cpus-per-task 1 -t 30 --mem-per-cp
 salloc -p gpu_4 --gres=gpu:1 --nodes 1 --ntasks 2 --ntasks-per-node 2 --cpus-per-task 1 -t 02:00:00 --mem-per-cpu 8000
 salloc -p gpu_8 --gres=gpu:1 --nodes 1 --ntasks 2 --ntasks-per-node 2 --cpus-per-task 1 -t 02:00:00 --mem-per-cpu 8000
 
+cd ~/hpc_tests/mpi4py_cupy/
 conda activate test_mpi4py_cupy
 module purge
 module load devel/cudnn/9.2

679634f399581b4cbfe55dd763c82786468079bb
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-07-01 12:18:04 +0200

    modified mpi4jax environment

--- mpi4jax/create_env.sh
+++ mpi4jax/create_env.sh
@@ -10,6 +10,5 @@ module load mpi/openmpi/4.1
 
 pip install mpi4py --no-binary mpi4py
 pip install --upgrade "jax[cuda]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
-pip cache purge
 CUDA_ROOT=/opt/bwhpc/common/devel/cuda/11.4 pip install mpi4jax --no-build-isolation
 pip cache purge

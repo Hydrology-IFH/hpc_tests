7c55b32d16a81b5a17fefc531ecb5ec89ac23b3b
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-08-24 15:31:05 +0200

    modified tests for binac

--- binac/cupy/example7.py
+++ binac/cupy/example7.py
@@ -2,6 +2,7 @@ import cupy as cp
 import cupyx.scipy.fft as cufft
 import scipy.fft
 scipy.fft.set_global_backend(cufft)
+print(cp.show_config())
 
 a = cp.random.random(100).astype(cp.float64)
 b = scipy.fft.fft(a)

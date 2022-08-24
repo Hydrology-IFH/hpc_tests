1dcd3fdcab88744af2e0a7a2b05fcf857938b29c
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-08-24 22:56:18 +0200

    modified tests

--- binac/mpi4py_cupy/example10.py
+++ binac/mpi4py_cupy/example10.py
@@ -3,6 +3,5 @@ print(cp.show_config())
 print("CUDA Device: ", cp.cuda.runtime.getDeviceCount())
 
 sendbuf = cp.arange(0, 10, dtype=float)
-assert hasattr(sendbuf, '__cuda_array_interface__')
 
 print('Success')

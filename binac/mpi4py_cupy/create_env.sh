caac16d04b235af11c0c9a7a14130f95184edf03
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-08-24 14:56:52 +0200

    modified tests for binac

--- binac/mpi4py_cupy/create_env.sh
+++ binac/mpi4py_cupy/create_env.sh
@@ -3,7 +3,8 @@ conda env create -f conda-environment.yml
 conda clean -a
 conda activate test_mpi4py_cupy
 module purge
-module load mpi/openmpi/4.1-gnu-9.2
+module load mpi/openmpi/4.1-gnu-9.2-cuda-11.4
+module load lib/hdf5/1.12.0-openmpi-4.1-gnu-9.2
 
 pip install mpi4py --no-binary mpi4py
 pip install cupy-cuda114

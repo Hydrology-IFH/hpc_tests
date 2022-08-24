e0e8185b48f0980bd87e55f7c78dcb125c7969fc
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-08-24 18:16:05 +0200

    modified tests for binac

--- binac/mpi4py_cupy/example4.py
+++ binac/mpi4py_cupy/example4.py
@@ -1,12 +1,12 @@
 from mpi4py import MPI
 import cupy
-import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-print(cupy.show_config())
 
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
+
+if rank == 0:
+    print(cupy.show_config())
 
 # # Allreduce
 # sendbuf = cupy.arange(10, dtype='i')

f528e85370b1fa5f538f296c7c6502b469916a4e
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-08-24 15:30:53 +0200

    modified tests for binac

--- binac/cupy/example7.py
+++ binac/cupy/example7.py
@@ -3,6 +3,6 @@ import cupyx.scipy.fft as cufft
 import scipy.fft
 scipy.fft.set_global_backend(cufft)
 
-a = cp.random.random(100).astype(cp.complex64)
+a = cp.random.random(100).astype(cp.float64)
 b = scipy.fft.fft(a)
 print('Success')
--- binac/cupy/example8.py
+++ binac/cupy/example8.py
@@ -1,12 +1,12 @@
 import cupy as cp
-
+print(cp.show_config())
 size = cp.cuda.runtime.getDeviceCount()
 assert size == 2  # make sure we are on 2 GPUs
 cp.fft.config.use_multi_gpus = True
 cp.fft.config.set_cufft_gpus([0, 1])  # use GPU 0 & 1
 
 shape = (64, 64)  # batch size = 64
-dtype = cp.complex64
+dtype = cp.float64
 a = cp.random.random(shape).astype(dtype)  # reside on GPU 0
 
 b = cp.fft.fft(a)  # computed on GPU 0 & 1, reside on GPU 0

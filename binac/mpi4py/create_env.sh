8d1ae8ed907e1e0460986324d9dadb8b3e42d266
schwemro <robin.schwemmle@hydrology.uni-freiburg.de>
2022-10-24 20:34:55 +0200

    added further tests

--- binac/mpi4py/create_env.sh
+++ binac/mpi4py/create_env.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 conda env create -f conda-environment.yml
 conda clean -a
-conda activate test_mpi4py_cupy
+conda activate test_mpi4py
 module purge
 module load lib/hdf5/1.12.0-openmpi-4.1-gnu-9.2
-module load lib/cudnn/8.2-cuda-11.4
 
 pip install mpi4py --no-binary mpi4py
-pip install cupy-cuda114
-pip cache purge
---
+++ binac/mpi4py/example1.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#PBS -l nodes=1:ppn=2
+#PBS -l walltime=00:10:00
+#PBS -l pmem=4000mb
+#PBS -N test
+#PBS -m bea
+#PBS -M robin.schwemmle@hydrology.uni-freiburg.de
+
+# load module dependencies
+module purge
+module load lib/hdf5/1.12.0-openmpi-4.1-gnu-9.2
+export OMP_NUM_THREADS=1
+eval "$(conda shell.bash hook)"
+conda activate test_mpi4py
+cd /home/fr/fr_fr/fr_rs1092/hpc_tests/binac/mpi4py
+mpirun -n 2 python example1.py
